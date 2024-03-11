62db909550957cca2f465752513d63bf844d8672
Antonio <contact@goweki.com>
2024-03-12 02:53:35 +0300

    init

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
             href="https://goweki.com"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+            <span className="sm:ml-2 self-center text-2xl font-semibold whitespace-nowrap text-white">
               INTRAKO
             </span>
           </Link>
