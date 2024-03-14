450cbab7590d981beca0b50617304306c59494f4
Antonio <contact@goweki.com>
2024-03-14 23:41:07 +0300

    init

--- src/components/atoms/heroCard.js
+++ src/components/atoms/heroCard.js
@@ -7,7 +7,7 @@ export default function HeroCard() {
       className="z-10 text-center px-4 py-8 rounded shadow-md lg:shadow-transparent bg-blue-100/50 backdrop-blur-md lg:bg-transparent max-w-sm top-1/3 lg:top-1/4 left-1/2 lg:left-3/4 absolute transform -translate-x-1/2"
     >
       <div className="animate-fade">
-        <h1 className="animate-up-in text-center text-sm md:text-2xl lg:text-3xl xl:text-4xl font-display font-light mb-6">
+        <h1 className="animate-up-in text-center text-sm md:text-2xl lg:text-3xl xl:text-4xl font-display mb-6">
           Your partner in marketing and business development
         </h1>
         <Link
