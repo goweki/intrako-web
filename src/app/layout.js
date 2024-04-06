74f3827eb2c3529628390ae23a88c2b7a010c04b
Antonio <contact@goweki.com>
2024-04-06 18:46:46 +0300

    ...

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} antialiased bg-grey-100 text-gray-800 tracking-tight`}
-      >
+      <body className={`${inter.className} antialiased tracking-tight`}>
         <Providers>
           <div className="flex flex-col min-h-screen overflow-hidden">
             <Header />
